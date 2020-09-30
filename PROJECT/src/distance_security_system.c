a128f51abb9e6430a05344e92839add447b64ba7
Jeyky <vovayeslenlo@gmail.com>
2020-09-30 18:47:09 +0300

    some rcc_clock_enable thing

--- PROJECT/src/distance_security_system.c
+++ PROJECT/src/distance_security_system.c
@@ -214,9 +214,13 @@ double get_speed_of_sound(struct sk_dht11 *dht11)
 
 int main(void)
 {
-	rcc_periph_clock_enable(RCC_GPIOE);
-	rcc_periph_clock_enable(RCC_GPIOD);
 	
+	
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOB);
+	rcc_periph_clock_enable(RCC_GPIOC);
+	rcc_periph_clock_enable(RCC_GPIOD);
+	rcc_periph_clock_enable(RCC_GPIOE);
 
 	glsk_pins_init(false);
 
